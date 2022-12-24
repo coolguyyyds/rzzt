c8538b638955433626b04bf40ecafa3b2df80675
wrk <wangruikaihaha@163.com>
2022-12-24 17:01:06 +0800

    初始化项目

---
+++ src/api/user.js
@@ -0,0 +1,11 @@
+import request from '@/utils/request'
+
+export function login(data) {
+  return request({
+  })
+}
+
+export function getInfo(token) {
+  return request({
+  })
+}
---
+++ src/views/learn-comp/index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>组件间通信</div>
+</template>
+
+<script>
+export default {
+  name: 'LearnComp'
+}
+</script>
+
+<style>
+</style>
